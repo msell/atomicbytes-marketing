661055f2347ee8fa1d2fc6492d859a5d24388b93
Matt Sell <mwsell@gmail.com>
2019-02-07 22:11:28 -0600

    init

---
+++ src/components/Header.js
@@ -0,0 +1,80 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import {Helmet} from "react-helmet";
+
+const Header = props => (
+  <header id="header" style={props.timeout ? { display: 'none' } : {}}>
+    <div className="atom">
+      <div className="electron" />
+      <div className="electron" />
+      <div className="electron" />
+    </div>
+    <div className="content">
+    <Helmet>
+        <link href="https://fonts.googleapis.com/css?family=Major+Mono+Display|Roboto" rel="stylesheet" />
+    </Helmet>
+      <div className="inner">
+      <div className="typewriter">
+        <h1 className="title">Atomic Bytes</h1>
+    </div>
+        <p>
+          Custom Software Solutions and Professional Services
+          {/* <br />
+          for free under the{' '}
+          <a href="https://html5up.net/license">Creative Commons</a> license. */}
+        </p>
+      </div>
+    </div>
+    <nav>
+      <ul>
+        <li>
+          <a
+            href="javascript:;"
+            onClick={() => {
+              props.onOpenArticle('intro')
+            }}
+          >
+            Intro
+          </a>
+        </li>
+        <li>
+          <a
+            href="javascript:;"
+            onClick={() => {
+              props.onOpenArticle('work')
+            }}
+          >
+            Work
+          </a>
+        </li>
+        <li>
+          <a
+            href="javascript:;"
+            onClick={() => {
+              props.onOpenArticle('about')
+            }}
+          >
+            About
+          </a>
+        </li>
+        <li>
+          <a
+            href="javascript:;"
+            onClick={() => {
+              props.onOpenArticle('contact')
+            }}
+          >
+            Contact
+          </a>
+        </li>
+      </ul>
+    </nav>
+  </header>
+)
+
+Header.propTypes = {
+  onOpenArticle: PropTypes.func,
+  timeout: PropTypes.bool,
+}
+
+export default Header
